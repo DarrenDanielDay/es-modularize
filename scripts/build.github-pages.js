74531f1d600d5418f195ca0dd8598f325ecaba5f
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-10-21 21:33:06 +0800

    demo: add index page

--- scripts/build.github-pages.js
+++ scripts/build.github-pages.js
@@ -1,3 +1,4 @@
+// @ts-check
 import { readFile, writeFile } from "fs/promises";
 import { join } from "path";
 
@@ -5,11 +6,18 @@ import { join } from "path";
 const PUBLIC_URL = process.env.PUBLIC_URL ?? "";
 const kinds = ["angular", "react"];
 const files = ["index.html", "full-load.html"];
-const targets = kinds.flatMap((k) => files.flatMap((f) => join(process.cwd(), "demo", k, f)));
+const targets = kinds.flatMap((k) => files.flatMap((f) => demoPath(k, f)));
+targets.push(demoPath("index.html"));
 for (const target of targets) {
   const html = await readFile(target, { encoding: "utf-8" });
   await writeFile(
     target,
     html.replace(/(?<=base href=")([^"]*)/g, (_, subPath) => `${PUBLIC_URL}${subPath}`)
   );
+}
+/**
+ * @param  {string[]} fragments path fragments
+ */
+function demoPath(...fragments) {
+  return join(process.cwd(), "demo", ...fragments);
 }
