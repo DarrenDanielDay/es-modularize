d066e2fd842a04cc517600b2624a0b7a2c94e409
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-27 01:27:46 +0800

    fix: remove `vue` jsx with `try-catch`
    For projects using `es-modularize`, `post-install` script is useless.

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,9 +2,13 @@
 import { readFile, writeFile } from "fs/promises";
 console.log("post install: remove vue jsx");
 const file = "./node_modules/@vue/runtime-dom/dist/runtime-dom.d.ts";
-const content = await readFile(file, { encoding: "utf-8" });
-const noJSX = content
-  .split(/\r?\n/g)
-  .filter((_, line) => !(1493 <= line && line <= 1509))
-  .join("\n");
-await writeFile(file, noJSX);
+try {
+  const content = await readFile(file, { encoding: "utf-8" });
+  const noJSX = content
+    .split(/\r?\n/g)
+    .filter((_, line) => !(1493 <= line && line <= 1509))
+    .join("\n");
+  await writeFile(file, noJSX);
+} catch (error) {
+  console.log("vue jsx setup ignored.");
+}
