6827e72a7ae6c6fb81b650f7c66920ca0aca02d0
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-27 04:34:53 +0800

    fix: `pos-install` for demo with `try-catch`

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -4,10 +4,12 @@ import { resolve } from "path";
 import { promisify } from "util";
 const exec = promisify(_exec);
 const cwd = process.cwd();
-await Promise.all(
-  ["angular", "react", "vue"].map(async (demo) => {
-    await exec("npm install", {
-      cwd: resolve(cwd, "demo", demo),
-    });
-  })
-);
+try {
+  await Promise.all(
+    ["angular", "react", "vue"].map(async (demo) => {
+      await exec("npm install", {
+        cwd: resolve(cwd, "demo", demo),
+      });
+    })
+  );
+} catch {}
